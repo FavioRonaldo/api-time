4b0377319362602c5c11db9c8faeeb2355d88d15
FavioRonaldo <50154174+FavioRonaldo@users.noreply.github.com>
2023-03-13 23:06:32 -0500

    limpieza de codigo

--- src/api-tiempo-data/api-tiempo-data.controller.ts
+++ src/api-tiempo-data/api-tiempo-data.controller.ts
@@ -3,22 +3,13 @@ import { SrvApiTiempoService } from '../srv-api-tiempo/srv-api-tiempo.service';
 import { HttpService } from '@nestjs/axios';
 import OpenWeatherAPI from 'openweather-api-node';
 
-
 @Controller('api-tiempo-data')
 export class ApiTiempoDataController {
   constructor(
-    private http: HttpService,
+    //private http: HttpService,
     private readonly itemsService: SrvApiTiempoService,
   ) {}
-  items: any[];
-  /*@Get(':city')
-  async getWeather(@Param() params) {
-    const apiKey = 'd92d754701ff867a897adddd0e4bfb85';
-    const url = `${this.apiUrl}?q=${params.city}&appid=${apiKey}`;
-    return `Estás consultando el producto ${params.city}`;
-    //const data = await this.itemsService.getSomeData(url);
-    //return data;
-  }*/
+//This function makes the call to the Open Weatherm API through the OpenWeatherAPI library
   @Get(':city')
   async findAll(@Param() params, @Response() res) {
     const weather = new OpenWeatherAPI({
@@ -26,29 +17,11 @@ export class ApiTiempoDataController {
       locationName: `${params.city}`,
       units: 'metric',
     });
-   /* weather.getCurrent().then((data) => {
+    /* weather.getCurrent().then((data) => {
       return res.json(data);
     });*/
-    weather.getForecast().then((data) => { 
-    /* const table = [];
-      const today = new Date().getDay();
-      data.forEach((e, i) => {
-        // eslint-disable-next-line prefer-const
-        let newE = {};
-        newE['day'] = days[today + i > 6 ? today + i - 7 : today + i];
-        newE['temp'] = `${e.weather.temp.cur}\u00B0F`;
-        newE['rain'] = `${e.weather.rain}mm`;
-        newE['description'] = e.weather.description;
-        table.push(newE);
-      });*/
+    weather.getForecast().then((data) => {
       return res.json(data);
     });
-    /*getDailyForecast().then((data) => {
-      return res.json(data);
-    });*/
-
-    //const data = await this.itemsService.getSomeData(url);
-    //return `Estás consultando el producto ${url}`;
-    //return this.itemsService.findAll();
   }
 }
--- src/srv-api-tiempo/srv-api-tiempo.service.ts
+++ src/srv-api-tiempo/srv-api-tiempo.service.ts
@@ -1,19 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { Observable } from 'rxjs';
+
 @Injectable()
 export class SrvApiTiempoService {
-  constructor(private httpService: HttpService) {}
-
-  getSomeData(url): Observable<any> {
-    return this.httpService.get(url);
-  }
-  private items = [
-    { id: 1, name: 'Item 1' },
-    { id: 2, name: 'Item 2' },
-    { id: 3, name: 'Item 3' },
-  ];
-  findAll(): any[] {
-    return this.items;
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
 }
